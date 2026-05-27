d59acc1dfbc7aa882e32af87d887bfd100e2d09f
HarshVirani914 <harshvirani.91@gmail.com>
2026-05-27 20:23:57 +0200

    feat: add new sitemap entries for projects and about pages with updated metadata

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,5 +11,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
+    {
+      url: `${baseUrl}/projects`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
   ];
 }
